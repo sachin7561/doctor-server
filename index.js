f8e1073524db527ab223a8c09dc55efbde0f4cc4
unknown <sachins7561@gmail.com>
2023-10-10 15:42:57 +0530

    appointment-server

---
+++ index.js
@@ -0,0 +1,23 @@
+//1 import json server
+const jsonserver=require('json-server');
+
+//2 create a server application using jsonserver
+const server=jsonserver.create();
+
+//3 set up path for db.json file
+const router=jsonserver.router('db.json');
+
+//4 return a middleware used by the jsonserver
+const middleware=jsonserver.defaults();
+
+//5 setup a port number
+const port=process.env.PORT||3001
+
+//6 use in server
+server.use(middleware)
+server.use(router)
+
+//7 to run the post
+server.listen(port,()=>{
+    console.log('listening on port'+port);
+})
